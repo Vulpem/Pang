e0e19ddba2033b9f6a977a99af50b3c80a57ef9f
Vulpem <davidher96@gmail.com>
2015-03-04 12:14:29 +0100

    Clase

---
+++ Pang/Main.cpp
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "SDL.h"
+
+/*
+#include "SDL/include/SDL.h"
+#pragma comment(lib, "SDL/lib/x86/SDL2.lib")
+#pragma comment(lib, "SDL/lib/x86/SDL2main.lib")
+*/
+
+
+int main(int argc, char** argv)
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	
+
+
+	SDL_Quit();
+	return 0;
+}
