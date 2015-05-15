39ad5df8bc2be2d1701c63355345e2d8c584ba5e
markitus18 <marc18.garrigo@gmail.com>
2015-05-15 23:48:50 +0200

    Commitment

--- Pang/ModuleFonts.cpp
+++ Pang/ModuleFonts.cpp
@@ -1,25 +1,31 @@
-
-//#include "ModuleFonts.h"
-/*
-#include "Application.h"
 #include "Globals.h"
+#include "Application.h"
+#include "ModuleFonts.h"
 
 ModuleFonts::ModuleFonts(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
-
 }
 
 ModuleFonts::~ModuleFonts()
-{
+{}
 
-}
-
-bool ModuleFonts::Start()
+bool ModuleFonts::Init()
 {
 	return true;
 }
 
+update_status ModuleFonts::PreUpdate()
+{
+	return UPDATE_CONTINUE;
+}
+
 update_status ModuleFonts::Update()
+{
+	return UPDATE_CONTINUE;
+}
+
+// PostUpdate present buffer to screen
+update_status ModuleFonts::PostUpdate()
 {
 	return UPDATE_CONTINUE;
 }
@@ -28,4 +34,3 @@ bool ModuleFonts::CleanUp()
 {
 	return true;
 }
-*/
--- Pang/ModuleFonts.h
+++ Pang/ModuleFonts.h
@@ -1,33 +1,17 @@
 #pragma once
-
 #include "Module.h"
 #include "Globals.h"
-
-//#include "SDL2_ttf-2.0.12\include\SDL_ttf.h"
 
 class ModuleFonts : public Module
 {
 public:
-
 	ModuleFonts(Application* app, bool start_enabled = true);
 	~ModuleFonts();
 
-
-
-	bool Start();
+	bool Init();
+	update_status PreUpdate();
 	update_status Update();
+	update_status PostUpdate();
 	bool CleanUp();
-
-	SDL_Surface* screen;
-	SDL_Surface* fontSurface;
-	SDL_Color fColor;
-	SDL_Rect fontRect;
-	TTF_Font* font;
-
-	void FontInit();
-
-	void printF(char* c, int x, int y);
-
 public:
-
 };
